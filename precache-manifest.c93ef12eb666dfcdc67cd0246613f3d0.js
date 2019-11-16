c61b7980b12702262cb0c55709de3db52eb2a58e
Yurii Martynenko <ymartinenko4@gmail.com>
2019-11-16 21:19:30 +0200

    Updates

---
+++ precache-manifest.c93ef12eb666dfcdc67cd0246613f3d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd8a9bda5b4c907a7f2c827d75ab345a",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "340aa26b36bbeca7328e",
+    "url": "/react-todo/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "5630d85e6331957677a0",
+    "url": "/react-todo/static/css/main.7c1d7f2a.chunk.css"
+  },
+  {
+    "revision": "340aa26b36bbeca7328e",
+    "url": "/react-todo/static/js/2.bb890719.chunk.js"
+  },
+  {
+    "revision": "5630d85e6331957677a0",
+    "url": "/react-todo/static/js/main.ae7fd0fb.chunk.js"
+  },
+  {
+    "revision": "481c40a7f0f41a057b67",
+    "url": "/react-todo/static/js/runtime-main.33ae4576.js"
+  }
+]);
